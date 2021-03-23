9c8e510b91efd943bbcce88688cb5ba5a3812fa0
Rachael <74115477+rachaeltc@users.noreply.github.com>
2021-03-23 16:55:10 -0400

    added profile sidebar category formatting

--- src/frontend/pages/profile.js
+++ src/frontend/pages/profile.js
@@ -10,12 +10,19 @@ export default function Home() {
         <title>Luma | Profile</title>
         <meta name="keywords" content="luma" />
       </Head>
-      <div className={styles.banner}>
-         <p> [ Insert Image Here] </p>
-      </div>
+      <div className={styles.banner} />
       <div className={styles.profilebar} />
+
       <div className={styles.biodrop}>
          <Image className={styles.pfp} src="/defaultpfp.jpg" width={200} height={200}/> 
+
+         {/* Add wave button here*/}
+
+         <h3 className={styles.category}> biography </h3>
+
+         <h3 className={styles.category}> experience </h3>
+
+         <h3 className={styles.category}> contact information </h3>
       </div>
     </>
   );
