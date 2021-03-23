b4ef1632f9590a2b545126c29307a099b491aa2e
Rachael <74115477+rachaeltc@users.noreply.github.com>
2021-03-22 21:06:42 -0400

    modified navbar and icons

--- src/frontend/components/navbar.js
+++ src/frontend/components/navbar.js
@@ -7,27 +7,33 @@ const Navbar = () => {
 
     return ( 
         <nav>
-            <div className="logo">
-                <Link href="/">
-                    <a><Image src="/logo.png" width={78} height={35}/></a>
-                </Link>
-            </div>
-            <div>
-                <input 
+        <div className="leftel">
+        
+        <Link href="/">
+                <a><Image src="/logo.png" width={78} height={35}/></a>
+        </Link>
+        
+            <input 
                 className="searchBar"
                 placeholder="Search here..."/>
-            </div>
+
+        </div>
+        <div class="rightel">
             <a> 
+                
                 <Link href="/connections">
                     <Image src="/connect.png" width={41} height={35}/>
                 </Link>
+                &emsp;
                 <Link href="/profile">
                     <Image src="/account.png" width={23} height={35}/>
                 </Link>
-                <Link href="/settings">
-                    <Image src="/settings.png" width={37} height={35}/>
+                &emsp;
+                <Link href="/login">
+                    <Image src="/login.png" width={36} height={35}/>
                 </Link>
             </a>
+        </div>
         </nav>
      );
 }
