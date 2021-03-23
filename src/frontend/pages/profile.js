f1c29e17395e84caccd35945461c309ba366d274
Rachael <74115477+rachaeltc@users.noreply.github.com>
2021-03-23 16:39:49 -0400

    initial layout setup

--- src/frontend/pages/profile.js
+++ src/frontend/pages/profile.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import Footer from "../components/footer";
-import Navbar from "../components/navbar";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/profile.module.css";
+import Image from 'next/image'
 import Link from "next/link";
 
 export default function Home() {
@@ -11,16 +10,12 @@ export default function Home() {
         <title>Luma | Profile</title>
         <meta name="keywords" content="luma" />
       </Head>
-      <div>
-        <h1 className={styles.title}>Profile Page</h1>
-        <p1 className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p1>
-        <div></div>
-        <p1 className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p1>
-        <div></div>
-        <p1 className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p1>
-        <Link href="/about">
-          <a className={styles.btn}>Who are we?</a>
-        </Link>
+      <div className={styles.banner}>
+         <p> [ Insert Image Here] </p>
+      </div>
+      <div className={styles.profilebar} />
+      <div className={styles.biodrop}>
+         <Image className={styles.pfp} src="/defaultpfp.jpg" width={200} height={200}/> 
       </div>
     </>
   );
